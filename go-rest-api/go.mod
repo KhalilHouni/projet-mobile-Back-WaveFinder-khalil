880ce446edcf67818d6aac4ba2ff0b2187b8d4c3
KhalilHouni <132214784+KhalilHouni@users.noreply.github.com>
2024-05-20 21:05:05 +0200

    APi complete
    GET/POST/PUT/DELETE works !

--- go-rest-api/go.mod
+++ go-rest-api/go.mod
@@ -1,3 +1,5 @@
 module API
 
 go 1.22.3
+
+require github.com/gorilla/mux v1.8.1
--- go-rest-api/main.go
+++ go-rest-api/main.go
@@ -1,3 +1,4 @@
+// API Surf Spots by Khalil Houni
 package main
 
 import (
@@ -5,22 +6,24 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
+
+	"github.com/gorilla/mux"
 )
 
-// Spot représente la structure du fichier JSON
+// Définition des structures de données
+
 type Spot struct {
 	Records []SpotRecord `json:"records"`
 	Offset  string       `json:"offset"`
 }
 
-// SpotRecord représente un enregistrement dans le fichier JSON
 type SpotRecord struct {
 	ID          string     `json:"id"`
 	Fields      SpotFields `json:"fields"`
 	CreatedTime string     `json:"createdTime"`
 }
 
-// SpotFields représente les champs de l'enregistrement Spot
 type SpotFields struct {
 	SurfBreak               []string    `json:"Surf Break"`
 	DifficultyLevel         int         `json:"Difficulty Level"`
@@ -35,7 +38,6 @@ type SpotFields struct {
 	Address                 string      `json:"Address"`
 }
 
-// SpotPhoto représente une photo dans les champs Photos de l'enregistrement SpotFields
 type SpotPhoto struct {
 	ID         string          `json:"id"`
 	URL        string          `json:"url"`
@@ -45,42 +47,293 @@ type SpotPhoto struct {
 	Thumbnails SpotPhotoThumbs `json:"thumbnails"`
 }
 
-// SpotPhotoThumbs représente les miniatures d'une photo
 type SpotPhotoThumbs struct {
 	Small SpotThumbnail `json:"small"`
 	Large SpotThumbnail `json:"large"`
 	Full  SpotThumbnail `json:"full"`
 }
 
-// SpotThumbnail représente une miniature d'une photo
 type SpotThumbnail struct {
 	URL    string `json:"url"`
 	Width  int    `json:"width"`
 	Height int    `json:"height"`
 }
 
+// Fonctions utilitaires
+
+func readJSONFile(filename string) ([]byte, error) {
+	return os.ReadFile(filename)
+}
+
+func parseJSONData(data []byte, v interface{}) error {
+	return json.Unmarshal(data, v)
+}
+
+// Gestion des requêtes
+
 func getSpots(w http.ResponseWriter, r *http.Request) {
 	// Lecture du fichier JSON
-	jsonData, err := os.ReadFile("spot.json")
+	jsonData, err := readJSONFile("spot.json")
 	if err != nil {
+		log.Println("Error reading JSON file:", err)
 		http.Error(w, "Failed to read JSON file", http.StatusInternalServerError)
 		return
 	}
 
 	// Conversion du contenu JSON en une structure Spot
 	var spots Spot
-	err = json.Unmarshal(jsonData, &spots)
-	if err != nil {
+	if err := parseJSONData(jsonData, &spots); err != nil {
+		log.Println("Error parsing JSON data:", err)
 		http.Error(w, "Failed to parse JSON", http.StatusInternalServerError)
 		return
 	}
 
-	// Encodage de la structure Spot en JSON et envoi en réponse
+	// Création d'une liste simplifiée pour l'affichage dans la liste iOS
+	var simplifiedList []map[string]interface{}
+	for _, record := range spots.Records {
+		simplifiedSpot := map[string]interface{}{
+			"surfBreak": strings.Join(record.Fields.SurfBreak, ", "),
+			"photos":    record.Fields.Photos,
+			"address":   record.Fields.Address,
+		}
+		simplifiedList = append(simplifiedList, simplifiedSpot)
+	}
+
+	// Encodage de la liste simplifiée en JSON et envoi en réponse
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(spots)
+	if err := json.NewEncoder(w).Encode(simplifiedList); err != nil {
+		log.Println("Error encoding JSON response:", err)
+		http.Error(w, "Failed to encode JSON response", http.StatusInternalServerError)
+	}
+}
+
+func getOneSpot(w http.ResponseWriter, r *http.Request) {
+	// Récupération de l'ID du spot depuis les paramètres de la requête
+	vars := mux.Vars(r)
+	spotID := vars["id"]
+
+	// Lecture du fichier JSON
+	jsonData, err := readJSONFile("spot.json")
+	if err != nil {
+		log.Println("Error reading JSON file:", err)
+		http.Error(w, "Failed to read JSON file", http.StatusInternalServerError)
+		return
+	}
+
+	// Conversion du contenu JSON en une structure Spot
+	var spots Spot
+	if err := parseJSONData(jsonData, &spots); err != nil {
+		log.Println("Error parsing JSON data:", err)
+		http.Error(w, "Failed to parse JSON", http.StatusInternalServerError)
+		return
+	}
+
+	// Recherche du spot correspondant à l'ID spécifié
+	var foundSpot SpotRecord
+	for _, record := range spots.Records {
+		if record.ID == spotID {
+			foundSpot = record
+			break
+		}
+	}
+
+	// Vérification si le spot a été trouvé
+	if foundSpot.ID == "" {
+		http.Error(w, "Spot not found", http.StatusNotFound)
+		return
+	}
+
+	// Encodage du spot trouvé en JSON et envoi en réponse
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(foundSpot); err != nil {
+		log.Println("Error encoding JSON response:", err)
+		http.Error(w, "Failed to encode JSON response", http.StatusInternalServerError)
+	}
+}
+
+func addSpot(w http.ResponseWriter, r *http.Request) {
+	var newSpot SpotRecord
+
+	// Décodage de la requête JSON
+	if err := json.NewDecoder(r.Body).Decode(&newSpot); err != nil {
+		log.Println("Error decoding JSON request:", err)
+		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		return
+	}
+
+	// Lecture du fichier JSON
+	jsonData, err := readJSONFile("spot.json")
+	if err != nil {
+		log.Println("Error reading JSON file:", err)
+		http.Error(w, "Failed to read JSON file", http.StatusInternalServerError)
+		return
+	}
+
+	// Conversion du contenu JSON en une structure Spot
+	var spots Spot
+	if err := parseJSONData(jsonData, &spots); err != nil {
+		log.Println("Error parsing JSON data:", err)
+		http.Error(w, "Failed to parse JSON", http.StatusInternalServerError)
+		return
+	}
+
+	// Ajout du nouveau spot à la liste
+	spots.Records = append(spots.Records, newSpot)
+
+	// Encodage et sauvegarde de la nouvelle liste
+	updatedData, err := json.Marshal(spots)
+	if err != nil {
+		log.Println("Error encoding JSON data:", err)
+		http.Error(w, "Failed to encode JSON", http.StatusInternalServerError)
+		return
+	}
+
+	if err := os.WriteFile("spot.json", updatedData, 0644); err != nil {
+		log.Println("Error writing JSON file:", err)
+		http.Error(w, "Failed to write JSON file", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
+
+func updateSpot(w http.ResponseWriter, r *http.Request) {
+	// Récupération de l'ID du spot depuis les paramètres de la requête
+	vars := mux.Vars(r)
+	spotID := vars["id"]
+
+	// Décodage de la requête JSON pour les champs à mettre à jour
+	var updatedFields struct {
+		SurfBreak []string    `json:"Surf Break"`
+		Photos    []SpotPhoto `json:"Photos"`
+		Address   string      `json:"Address"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&updatedFields); err != nil {
+		log.Println("Error decoding JSON request:", err)
+		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		return
+	}
+
+	// Lecture du fichier JSON
+	jsonData, err := readJSONFile("spot.json")
+	if err != nil {
+		log.Println("Error reading JSON file:", err)
+		http.Error(w, "Failed to read JSON file", http.StatusInternalServerError)
+		return
+	}
+
+	// Conversion du contenu JSON en une structure Spot
+	var spots Spot
+	if err := parseJSONData(jsonData, &spots); err != nil {
+		log.Println("Error parsing JSON data:", err)
+		http.Error(w, "Failed to parse JSON", http.StatusInternalServerError)
+		return
+	}
+
+	// Recherche et mise à jour des champs du spot correspondant à l'ID spécifié
+	spotUpdated := false
+	for i, record := range spots.Records {
+		if record.ID == spotID {
+			if updatedFields.SurfBreak != nil {
+				spots.Records[i].Fields.SurfBreak = updatedFields.SurfBreak
+			}
+			if updatedFields.Photos != nil {
+				spots.Records[i].Fields.Photos = updatedFields.Photos
+			}
+			if updatedFields.Address != "" {
+				spots.Records[i].Fields.Address = updatedFields.Address
+			}
+			spotUpdated = true
+			break
+		}
+	}
+
+	// Vérification si le spot a été trouvé
+	if !spotUpdated {
+		http.Error(w, "Spot not found", http.StatusNotFound)
+		return
+	}
+
+	// Encodage et sauvegarde de la nouvelle liste
+	updatedData, err := json.Marshal(spots)
+	if err != nil {
+		log.Println("Error encoding JSON data:", err)
+		http.Error(w, "Failed to encode JSON", http.StatusInternalServerError)
+		return
+	}
+
+	if err := os.WriteFile("spot.json", updatedData, 0644); err != nil {
+		log.Println("Error writing JSON file:", err)
+		http.Error(w, "Failed to write JSON file", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func deleteSpot(w http.ResponseWriter, r *http.Request) {
+	// Récupération de l'ID du spot depuis les paramètres de la requête
+	vars := mux.Vars(r)
+	spotID := vars["id"]
+
+	// Lecture du fichier JSON
+	jsonData, err := readJSONFile("spot.json")
+	if err != nil {
+		log.Println("Error reading JSON file:", err)
+		http.Error(w, "Failed to read JSON file", http.StatusInternalServerError)
+		return
+	}
+
+	// Conversion du contenu JSON en une structure Spot
+	var spots Spot
+	if err := parseJSONData(jsonData, &spots); err != nil {
+		log.Println("Error parsing JSON data:", err)
+		http.Error(w, "Failed to parse JSON", http.StatusInternalServerError)
+		return
+	}
+
+	// Recherche du spot correspondant à l'ID spécifié
+	index := -1
+	for i, record := range spots.Records {
+		if record.ID == spotID {
+			index = i
+			break
+		}
+	}
+
+	// Vérification si le spot a été trouvé
+	if index == -1 {
+		http.Error(w, "Spot not found", http.StatusNotFound)
+		return
+	}
+
+	// Suppression du spot de la liste
+	spots.Records = append(spots.Records[:index], spots.Records[index+1:]...)
+
+	// Encodage et sauvegarde de la nouvelle liste
+	updatedData, err := json.Marshal(spots)
+	if err != nil {
+		log.Println("Error encoding JSON data:", err)
+		http.Error(w, "Failed to encode JSON", http.StatusInternalServerError)
+		return
+	}
+
+	if err := os.WriteFile("spot.json", updatedData, 0644); err != nil {
+		log.Println("Error writing JSON file:", err)
+		http.Error(w, "Failed to write JSON file", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
 }
 
 func main() {
-	http.HandleFunc("/api/spots", getSpots)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	r := mux.NewRouter()
+	r.HandleFunc("/api/spots", getSpots).Methods("GET")
+	r.HandleFunc("/api/spots/{id}", getOneSpot).Methods("GET")
+	r.HandleFunc("/api/spots", addSpot).Methods("POST")
+	r.HandleFunc("/api/spots/{id}", updateSpot).Methods("PUT")
+	r.HandleFunc("/api/spots/{id}", deleteSpot).Methods("DELETE")
+	log.Println("Server listening on :8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
